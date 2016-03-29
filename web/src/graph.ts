46de9b6f758d0696465b615ade675b35f6f552c5
Michal Wallace <michal.wallace@gmail.com>
2016-03-29 17:27:52 -0500

    Update the graph when the `data` attribute changes.

--- web/src/graph.ts
+++ web/src/graph.ts
@@ -1,5 +1,6 @@
 // draw a directed graph with d3
 import {ElementRef} from "angular2/core";
+import {SimpleChange} from "angular2/core";
 import {Directive} from "angular2/core";
 import {Inject} from "angular2/core";
 import {Input} from "angular2/core";
@@ -17,12 +18,19 @@ export class Graph {
   private svg: any; force: any; // d3 force layout
 
   constructor(@Inject(ElementRef) elRef) {
-    var svg = d3.select(elRef.nativeElement).append('svg').attr({width: '100%', height: '100%'});
-    var force = d3.layout.force().charge(-250).linkDistance(45).size([640, 480]);
-
     console.log('in the Graph constructor');
-    console.log(['this.data:', this.data]);
-    if (this.data == null) return;
+    this.svg = d3.select(elRef.nativeElement).append('svg').attr({width: '100%', height: '100%'});
+    this.force = d3.layout.force().charge(-250).linkDistance(45).size([640, 480]);
+  }
+
+  ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+    if (changes['data']) this.redraw()
+  }
+
+  redraw() {
+
+    var force = this.force;
+    var svg = this.svg;
 
     var data = this.data;
     force.nodes(data.nodes).links(data.edges).start();
